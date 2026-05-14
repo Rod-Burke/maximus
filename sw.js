65fbe9009b041f7d76e1becdbaa6ae1ee10fc3ea
Rod-Burke <fra_roderic@outlook.com>
2026-05-13 21:07:22 -0400

    Add clients.claim() to SW for immediate mobile PWA updates

--- sw.js
+++ sw.js
@@ -7,7 +7,11 @@ self.addEventListener('install', (e) => {
 });
 
 self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (e) => {
