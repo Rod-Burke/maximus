203d9f3b07ba6803d172aafd73c97ae3003a681b
Rod-Burke <fra_roderic@outlook.com>
2026-07-14 15:38:02 -0400

    Deploy: Maximus PWA Update - 2026-07-14 15:38:02

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'maximus-v127';
+const CACHE_NAME = 'maximus-v128';
 const ASSETS = ['index.html', 'style.css', 'app.js', 'manifest.json', 'icon.svg', 'migration.html', 'migration.js'];
 
 self.addEventListener('install', (e) => {
